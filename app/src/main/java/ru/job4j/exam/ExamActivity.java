77c25637177abdaae4ec1215f91bb04d843c47df
Roux13 <u.nehodov@gmail.com>
2020-03-19 23:06:57 +0300

    1. Основы Android / 1. Android Studio / 3. Создание проекта и запуск через эмулятор Genymotion[#256564]

---
+++ app/src/main/java/ru/job4j/exam/ExamActivity.java
@@ -0,0 +1,14 @@
+package ru.job4j.exam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam);
+    }
+}
