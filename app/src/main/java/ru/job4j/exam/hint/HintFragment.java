379b6821523e3d859100c525b6b1fcff0f7360f8
Roux13 <u.nehodov@gmail.com>
2020-05-30 00:26:52 +0300

    test

--- app/src/main/java/ru/job4j/exam/hint/HintFragment.java
+++ app/src/main/java/ru/job4j/exam/hint/HintFragment.java
@@ -2,16 +2,15 @@ package ru.job4j.exam.hint;
 
 import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import ru.job4j.exam.R;
 import ru.job4j.exam.entitties.Question;
@@ -29,6 +28,7 @@ public class HintFragment extends Fragment {
     }
 
     public HintFragment() {
+
     }
 
     @Override
